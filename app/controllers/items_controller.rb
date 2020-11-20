9b2439f4e1201a46fe6eb34e22feb51c06e8a974
KimFight <mk_idea_lism@icloud.com>
2020-11-20 17:17:17 +0900

    itemモデルと画像モデルのアソシエーション（ちょっとitem_paramsも）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,10 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+private
+
+  def item_paramas
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
