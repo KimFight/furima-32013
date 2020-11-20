e138d94b6942768f6fab879dbd49245701290807
KimFight <mk_idea_lism@icloud.com>
2020-11-20 19:13:30 +0900

    categoryとstatusのモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
-belongs_to :user
-has_one_attached :image  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+
+  belongs_to :user
+  has_one_attached :image  
+
+  validates :category_id, numericality: { other_than: 1 }
+
 end
