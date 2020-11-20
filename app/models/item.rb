10d85d89a806349237b73f031e7a436813ef9583
KimFight <mk_idea_lism@icloud.com>
2020-11-20 19:18:47 +0900

    DeliveryBurdenモデルの入力

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :deliveryburden
 
   belongs_to :user
   has_one_attached :image  
