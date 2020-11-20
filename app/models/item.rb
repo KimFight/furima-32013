94952f4dbdfe872fdebc03081c339398f172928f
KimFight <mk_idea_lism@icloud.com>
2020-11-20 20:57:41 +0900

    発送元と発送までの日数のActive_hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
-  belongs_to :deliveryburden
+  belongs_to :delivery_burden
+  belongs_to :shipment_source
+  belongs_to :day
 
   belongs_to :user
   has_one_attached :image  
