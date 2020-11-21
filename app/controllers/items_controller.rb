92d160d89653e5f3bb889c2655b96e9e4d52fc68
KimFight <mk_idea_lism@icloud.com>
2020-11-21 15:04:14 +0900

    days（配送日数）をdayに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,14 +9,19 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :new
+    end
   end
 
 
 
 private
 
-  def item_paramas
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :delivery_burden_id, :shipment_source_id, :days_id, :price)#.merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,17 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :delivery_burden
   belongs_to :shipment_source
-  belongs_to :day
+  belongs_to :days
 
   belongs_to :user
   has_one_attached :image  
-
-  validates :category_id, numericality: { other_than: 1 }
-
+  
+  with_options presence: true do
+      validates :category_id,     numericality: { other_than: 1 }
+      validates :status_id,      numericality: { other_than: 1 }
+      validates :delivery_burden_id,  numericality: { other_than: 1 }
+      validates :shipment_source_id, numericality: { other_than: 1 }
+      validates :days_id, numericality: { other_than: 1 }
+      validates :image
+    end
 end
