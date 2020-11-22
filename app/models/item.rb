0058d9eb0ca4eea7026ead8d56afda0c32cb5909
KimFight <mk_idea_lism@icloud.com>
2020-11-22 12:09:15 +0900

    itemsモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,16 @@ class Item < ApplicationRecord
   has_one_attached :image  
   
   with_options presence: true do
-      validates :category_id,         numericality: { other_than: 1 }
-      validates :status_id,           numericality: { other_than: 1 }
-      validates :delivery_burden_id,  numericality: { other_than: 1 }
-      validates :shipment_source_id,  numericality: { other_than: 1 }
-      validates :day_id,              numericality: { other_than: 1 }
-      validates :image
-      validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000}
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_burden_id
+      validates :shipment_source_id
+      validates :day_id
     end
+      validates :image
+      validates :explanation
+      validates :name
+      validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000}
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name                {"test"}
+    explanation         {"test-test"}
+    category_id         {"2"}
+    status_id           {"2"}
+    delivery_burden_id  {"2"}
+    shipment_source_id  {"2"}
+    day_id              {"2"}
+    price               {"300"}
+    user_id             {"2"}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
