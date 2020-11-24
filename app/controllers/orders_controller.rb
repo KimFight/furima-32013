02bb4775efdf0074f13475055b1ed062f563bf6d
KimFight <mk_idea_lism@icloud.com>
2020-11-24 20:11:08 +0900

    テストコード前のコミット

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
 
 def index
+  
   @order_purchase = OrderPurchase.new
   @item = Item.find(params[:item_id])
+    
+    if current_user.id == @item.user_id
+       return redirect_to root_path
+    end
 end
 
 def create
   @item = Item.find(params[:item_id])
   @order_purchase = OrderPurchase.new(order_params)
-  if  @order_purchase.valid?
+  
+    if  @order_purchase.valid?
+
         @order_purchase.save
-         return redirect_to action: :index
+        return redirect_to root_path
+    
     else
-      render 'index'
+        render 'index'
     end
 end
 
