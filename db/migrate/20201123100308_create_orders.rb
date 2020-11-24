3becabfb5e0af4c56b12d0cba8e8b4ddd48ce20d
KimFight <mk_idea_lism@icloud.com>
2020-11-24 10:54:31 +0900

    orderテーブルpurchase テーブル作成(手順間違え)

--- db/migrate/20201123100308_create_orders.rb
+++ db/migrate/20201123100308_create_orders.rb
@@ -1,6 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
+      t.string       :postal_code,     null: false
+      t.integer      :prefectures_id,  null: false
+      t.string       :municipality,    null: false
+      t.string       :address,         null: false
+      t.string       :building_name
+      t.string       :phone_number,    null: false
 
       t.timestamps
     end
